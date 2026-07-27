f4c966f7a6f7ea090ad39e739907b3950a171df0
Prasanna-cpu <kumar22maran@gmail.com>
2026-07-27 08:36:08 +0530

    commit

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("Pipelines Hello")
+
+const search = require("./array-search")
+
+const flag = search.arraySearch([1, 2, 3], 2)
+
+if(flag === -1) console.log("Target not found")
+else console.log("Target found at index", flag)
+
